cd15c179aac301199f1d427d2c538e31ca228d8b
Jaya Priya R R <jaya2862000@gmail.com>
2021-09-03 18:34:09 +0530

    Main activity with completed bottom navigation

--- app/src/main/java/com/example/demolayout/MainActivity.java
+++ app/src/main/java/com/example/demolayout/MainActivity.java
@@ -4,17 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button to_secondActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         //Bottom Navigation
         BottomNavigationView btnNav=findViewById(R.id.bottomNavigationView);
