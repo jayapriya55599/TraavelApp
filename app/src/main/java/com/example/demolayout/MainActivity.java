fadc413b5c654b93858caa2d407239518dad31c3
Jaya Priya R R <jaya2862000@gmail.com>
2021-09-03 17:04:33 +0530

    Main activity with bottom navigation

--- app/src/main/java/com/example/demolayout/MainActivity.java
+++ app/src/main/java/com/example/demolayout/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.demolayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bottom Navigation
+        BottomNavigationView btnNav=findViewById(R.id.bottomNavigationView);
+        btnNav.setOnNavigationItemSelectedListener(navListener);
+
+
+        //Setting Home Fragment as main fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout, new HomeFragment()).commit();
     }
+
+
+    //Listen nav bar
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+
+            switch ((item.getItemId()))
+            {
+                case R.id.item1:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.item2:
+                    selectedFragment = new MyOrderFragment();
+                    break;
+                case R.id.item3:
+                    selectedFragment = new RewardFragment();
+                    break;
+
+                case R.id.item4:
+                    selectedFragment = new UserFragment();
+                    break;
+
+            }
+
+            //Begin Transaction
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout
+                    ,selectedFragment).commit();
+
+            return true;
+            }
+
+    };
 }
